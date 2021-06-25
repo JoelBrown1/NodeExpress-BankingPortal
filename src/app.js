75f6ff867a00c4589c882d18df88c81200def814
Joel Brown <joel.anthony.brown1@gmail.com>
2021-06-25 14:46:26 -0400

    Completed module 3

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+var bodyParser = require('body-parser')
 
 const port = process.env.PORT || 3000;
 
@@ -10,15 +11,13 @@ app.set('view engine', 'ejs');
 
 app.use('/css', express.static(path.join(__dirname, './public/css')));
 app.use('/js', express.static(path.join(__dirname, './public/js')));
-
+app.use(bodyParser.urlencoded({ extended: false }))
 const accountData = fs.readFileSync('src/json/accounts.json', {encoding:'utf8'});
 const accounts = JSON.parse(accountData);
-console.log('what is in accounts: ', accounts);
-const accountKeys = Object.keys(accounts);
+console.log('account information: ',accounts);
 
 const userData = fs.readFileSync('src/json/users.json', {encoding:'utf8'});
 const users = JSON.parse(userData);
-console.log('these are our users: ', users);
 
 app.get('/', (req, res) => {
   res.render('index', {
@@ -32,16 +31,43 @@ app.get('/savings', (req, res)=> {
     account: accounts.savings
   })
 });
+
 app.get('/checking', (req, res)=> {
   res.render('account', {
     account: accounts.checking
   })
 });
+
 app.get('/credit', (req, res)=> {
   res.render('account', {
     account: accounts.credit
   })
 });
+
+app.get('/transfer', (req, res)=> {
+  res.render('transfer')
+})
+  .post('/transfer', (req, res) => {
+    const {from, to, amount } = req.body;
+    const fromOriginalBalance = accounts[from].balance
+    const newFromBalance = fromOriginalBalance - amount;
+    accounts[from].balance = newFromBalance;
+
+    const newToBalance = accounts[to].balance + parseInt(amount);
+    accounts[to].balance = newToBalance;
+
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(
+      path.join(__dirname, './json/accounts.json'), 
+      accountsJSON, 
+      'utf8'
+    );
+    res.render('transfer', { message: 'Transfer Completed' })
+  });
+
+  app.get('/payment', (req, res) => {
+    res.render('payment', { account: accounts.credit })
+  })
 
 
 app.get(`/profile`, (req, res)=> {
